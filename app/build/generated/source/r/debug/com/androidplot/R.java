9dd11629ded2c3e6b2b4dca49d40e296e4741df5
Gautam Bose <Gautam@GautamsBPRetina.wv.cc.cmu.edu>
2017-03-02 06:03:23 -0500

    working no bpm

--- app/build/generated/source/r/debug/com/androidplot/R.java
+++ app/build/generated/source/r/debug/com/androidplot/R.java
@@ -8,22 +8,22 @@ package com.androidplot;
 
 public final class R {
 	public static final class attr {
-		public static final int domainLabel = 0x7f0100ad;
-		public static final int domainLabelTextSize = 0x7f0100ae;
-		public static final int graphMarginBottom = 0x7f0100b2;
-		public static final int graphMarginLeft = 0x7f0100b3;
-		public static final int graphMarginRight = 0x7f0100b4;
-		public static final int graphMarginTop = 0x7f0100b1;
-		public static final int plotLabel = 0x7f010032;
-		public static final int plotLabelTextSize = 0x7f010033;
-		public static final int rangeLabel = 0x7f0100af;
-		public static final int rangeLabelTextSize = 0x7f0100b0;
+		public static final int domainLabel = 0x7f0100b3;
+		public static final int domainLabelTextSize = 0x7f0100b4;
+		public static final int graphMarginBottom = 0x7f0100b8;
+		public static final int graphMarginLeft = 0x7f0100b9;
+		public static final int graphMarginRight = 0x7f0100ba;
+		public static final int graphMarginTop = 0x7f0100b7;
+		public static final int plotLabel = 0x7f010035;
+		public static final int plotLabelTextSize = 0x7f010036;
+		public static final int rangeLabel = 0x7f0100b5;
+		public static final int rangeLabelTextSize = 0x7f0100b6;
 	}
 	public static final class styleable {
-		public static final int[] Plot = { 0x7f010032, 0x7f010033 };
+		public static final int[] Plot = { 0x7f010035, 0x7f010036 };
 		public static final int Plot_plotLabel = 0;
 		public static final int Plot_plotLabelTextSize = 1;
-		public static final int[] xy_XYPlot = { 0x7f0100ad, 0x7f0100ae, 0x7f0100af, 0x7f0100b0, 0x7f0100b1, 0x7f0100b2, 0x7f0100b3, 0x7f0100b4 };
+		public static final int[] xy_XYPlot = { 0x7f0100b3, 0x7f0100b4, 0x7f0100b5, 0x7f0100b6, 0x7f0100b7, 0x7f0100b8, 0x7f0100b9, 0x7f0100ba };
 		public static final int xy_XYPlot_domainLabel = 0;
 		public static final int xy_XYPlot_domainLabelTextSize = 1;
 		public static final int xy_XYPlot_graphMarginBottom = 5;
--- app/src/main/java/works/com/hellovision2/VisionActivity.java
+++ app/src/main/java/works/com/hellovision2/VisionActivity.java
@@ -1,6 +1,8 @@
 package works.com.hellovision2;
 
-import org.apache.commons.*;
+
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -9,8 +11,14 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.SubMenu;
 import android.view.SurfaceView;
+import android.view.ViewDebug;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -27,20 +35,19 @@ import java.util.Calendar;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class VisionActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2
-{
+public class VisionActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     private final String TAG = "APP";
     private Tutorial3View mOpenCvCameraView;
     private SubMenu mColorEffectsMenu;
     long prevTs;
     TextView rgbVal;
-    float lastTouchY=0;
-    int cannyThreshold=50;
+    float lastTouchY = 0;
+    int cannyThreshold = 50;
 
     ArrayList<Float> greenValues;
     int bufferCounter = 0;
     int bufferSize = 100;
-    float sum=0;
+    float sum = 0;
     float halfWaySum = 0;
     Float ave = new Float(0);
     ArrayList<Double> bufferValues = new ArrayList<>();
@@ -49,19 +56,24 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
 
-                } break;
-                default:
-                {
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +89,9 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
         rgbVal = (TextView) findViewById(R.id.rgbVal);
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -124,20 +139,18 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
     public void onCameraViewStopped() {
     }
 
-    public float calculateMean(ArrayList<Float> values)
-    {
-        float mean= (float) 0.0;
-       for (int i=0;i<values.size();i++)
-       {
-           Log.d(TAG, values.get(i) + "");
-           mean += values.get(i);
+    public float calculateMean(ArrayList<Float> values) {
+        float mean = (float) 0.0;
+        for (int i = 0; i < values.size(); i++) {
+            Log.d(TAG, values.get(i) + "");
+            mean += values.get(i);
 
-       }
+        }
         mean /= values.size();
         return mean;
     }
 
-    public Double sumArrayList(ArrayList<Double> array){
+    public Double sumArrayList(ArrayList<Double> array) {
 
         Double sum = new Double(0);
         for (Double wef : array) {
@@ -165,33 +178,56 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
     public Double findMedian(ArrayList<Double> a) {
 
         Collections.sort(a);
-        if (a.size() == 0){
+        if (a.size() == 0) {
             return 0.0;
-        }
-
-        else if (a.size() % 2 == 1){
-            return a.get(a.size()/2);
-        }
-
-        else {
-            return (a.get(a.size()/2) + a.get(a.size()/2 - 1))/2.0;
+        } else if (a.size() % 2 == 1) {
+            return a.get(a.size() / 2);
+        } else {
+            return (a.get(a.size() / 2) + a.get(a.size() / 2 - 1)) / 2.0;
         }
     }
-    public ArrayList<Double> lowPassFilter(ArrayList<Double> deArray){
+
+    public ArrayList<Double> lowPassFilter(ArrayList<Double> deArray) {
         ArrayList<Double> filteredArray = new ArrayList<>();
 
         for (int index = 0; index < deArray.size(); index++) {
-            if(index > 5) {
-                Double medianValue = 
+            if (index > 1) {
+                ArrayList<Double> subListed = new ArrayList<Double>(deArray.subList(index - 1, index));
+                Double medianValue = findMedian(subListed);
+                filteredArray.add(medianValue);
+
             }
 
         }
+        return filteredArray;
     }
+
+    public int getBeats(ArrayList<Double> bufferValues) {
+        int beats = 0;
+        ArrayList<Double> bufferDeMeaned = deMeanList(bufferValues);
+        ArrayList<Double> filteredArray = lowPassFilter(bufferDeMeaned);
+
+        for (int filterIndex = 0; (filterIndex < filteredArray.size() - 1); filterIndex++) {
+            Double currentVal = filteredArray.get(filterIndex);
+            if (filterIndex > 2) {
+                Double prevVal = filteredArray.get(filterIndex - 1);
+                Double nextVal = filteredArray.get(filterIndex + 1);
+
+                if (prevVal < 0 && nextVal > 0) {
+                    beats++;
+
+                }
+            }
+        }
+        return beats / 2;
+
+    }
+
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat currentFrame = inputFrame.rgba();
 
-        long startTime = System.currentTimeMillis();
+//        long startTime = System.currentTimeMillis();
 
         //CANNY Edge Detection
         Imgproc.cvtColor(currentFrame, currentFrame, Imgproc.COLOR_RGBA2RGB);
@@ -204,9 +240,24 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
         bufferValues.add(valueMean);
 
 
-
         String valueMeanString = Double.toString(valueMean);
-        if (bufferValues.size() > 50) {
+        if (bufferValues.size() == 100) {
+            int currentBeats = getBeats(bufferValues);
+
+//            long stopTime = System.currentTimeMillis();
+//            long timeDifference = startTime - stopTime;
+
+            String beatsString = Integer.toString(currentBeats);
+//            String bufferValString = android.text.TextUtils.join(", ", bufferValues);
+//            String bufferValSize = Integer.toString(bufferValues.size());
+//            Log.d("wewef", bufferValSize);
+//            Log.d("wewef", bufferValString);
+//            Log.d("wewef", beatsString);
+
+            final TextView beatsView = (TextView) findViewById(R.id.rgbVal);
+            beatsView.setText(beatsString);
+            bufferValues.clear();
+
 
         }
 
@@ -231,5 +282,41 @@ public class VisionActivity extends ActionBarActivity implements CameraBridgeVie
         if (e.getAction() == MotionEvent.ACTION_UP)
             lastTouchY = 0;
         return true;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Vision Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
